c0dcd54850cbd09aec38d90e113a14886b34c5fa
joao <joao.fzm@hotmail.com>
2022-05-15 00:02:21 -0300

    Project created

---
+++ src/main/java/br/com/joaofzm15/fantasybasketball/FantasyBasketballLeagueManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaofzm15.fantasybasketball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyBasketballLeagueManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyBasketballLeagueManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joaofzm15/fantasybasketball/FantasyBasketballLeagueManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaofzm15.fantasybasketball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyBasketballLeagueManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
