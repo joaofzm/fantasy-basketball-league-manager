aa73c2b37a8dbc199ae71586cdd8c502f9dd823b
joao <joao.fzm@hotmail.com>
2022-05-23 23:47:46 -0300

    Add Cors configuration

---
+++ src/main/java/br/com/joaofzm15/fantasybasketball/config/CorsConfiguration.java
@@ -0,0 +1,23 @@
+package br.com.joaofzm15.fantasybasketball.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+@RestController
+public class CorsConfiguration {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+}
