893a13e2e91e14465b981e8c6e0c339093f7c509
joao <joao.fzm@hotmail.com>
2022-05-26 00:29:44 -0300

    Make login system fully functional, updated homepage screen

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -1,4 +1,11 @@
-function login() {
+
+function loadWelcomePage(){
+    currentLoggedTeamId = localStorage.getItem("currentLoggedTeamId");
+    document.getElementById("welcome_message").innerHTML = "Welcome, "+localStorage.getItem("currentlyLoggedUsername")+"!";
+    document.getElementById("current_team_label").innerHTML = "Owned team: "+localStorage.getItem("currentLoggedTeamName")+"!";
+}
+
+function logIn() {
     var typedUsername = document.getElementById("usernameTextBox").value;
     var typedPassword = document.getElementById("passwordTextBox").value;
 
@@ -9,15 +16,28 @@ function login() {
 
     request.onreadystatechange = function () {
         if (request.readyState === 4) {
-            let requestResponseText = request.responseText;
-            let parsedData = JSON.parse(requestResponseText);
-            for (let i = 0; i < parsedData.length; i++){
+            var requestResponseText = request.responseText;
+            var parsedData = JSON.parse(requestResponseText);
+
+            var matched = false;
+            for (var i = 0; i < parsedData.length; i++){
                 if (typedUsername == parsedData[i].username && typedPassword == parsedData[i].password){
-                    alert("Welcome, "+parsedData[i].username+"!");
+                    localStorage.setItem("currentlyLoggedUsername", parsedData[i].username);
+                    localStorage.setItem("currentLoggedTeamName", parsedData[i].teams[0].name);
+                    localStorage.setItem("currentLoggedTeamId", parsedData[i].teams[0].id);
+                    matched = true;
+                    window.location.href = "/pages/afterlogin.html";
                     break;
                 }
+            }
+            if (matched==false){
                 alert("Incorrect username or password!");
             }
         }
     }
+}
+
+function logOut(){
+    localStorage.clear();
+    window.location.href = "/index.html";
 }
