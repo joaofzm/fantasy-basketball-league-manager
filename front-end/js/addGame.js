040521f3c0b2cfe90b2bc6b0f509ba6a4295a8f7
joao <joao.fzm@hotmail.com>
2022-05-27 23:45:18 -0300

    Half implements addGame method, player ID is still mocked

--- front-end/js/addGame.js
+++ front-end/js/addGame.js
@@ -1,6 +1,43 @@
+function loadAddGamePage() {
+//Get all players from currently logged user's team, as an array
+    var request = new XMLHttpRequest();
+    request.open('get', "http://localhost:8080/teams/", true);
+    request.setRequestHeader('Content-Type', 'text/plain');
+    request.send();
+    request.onreadystatechange = function () {
+        if (request.readyState === 4) {
+            var requestResponseText = request.responseText;
+            var parsedData = JSON.parse(requestResponseText);
+
+            var players = [];
+
+            for (var i = 0; i < parsedData.length; i++) {
+                if (parsedData[i].id == localStorage.getItem("currentLoggedTeamId")) {
+                    for (var j = 0; j < parsedData[i].players.length; j++) {
+                        players.push(parsedData[i].players[j]);
+                    }
+                }
+            }
+
+            //Add the players from the array to the HTML select as options
+            for (var i = 0; i<  players.length; i++){
+                var option = document.createElement('option');
+                option.value = players[i].name;
+                option.innerHTML = players[i].name;
+                document.getElementById('playersSelect').appendChild(option);
+            }
+
+
+        }
+    }
+}
+
 function addGame() {
     event.preventDefault();
 
+    //Player ID is still mocked, TODO: Get right player id from select
+    var sel = document.getElementById("playersSelect").value;
+    
     var player = 1;
     var points = document.getElementById("pointsTextBox").value;
     var assists = document.getElementById("assistsTextBox").value;
@@ -35,41 +72,3 @@ function addGame() {
 
 
 
-
-
-
-
-function loadAddGamePage() {
-    getPlayers();
-}
-
-
-
-
-
-//Get all players from currently logged user's team, as an array
-function getPlayers() {
-    var request = new XMLHttpRequest();
-    request.open('get', "http://localhost:8080/teams/", true);
-    request.setRequestHeader('Content-Type', 'text/plain');
-    request.send();
-
-    request.onreadystatechange = function () {
-        if (request.readyState === 4) {
-            var requestResponseText = request.responseText;
-            var parsedData = JSON.parse(requestResponseText);
-
-            var players = [];
-
-            for (var i = 0; i < parsedData.length; i++) {
-                if (parsedData[i].id == localStorage.getItem("currentLoggedTeamId")) {
-                    for (var j = 0; j < parsedData[i].players.length; j++) {
-                        players.push(parsedData[i].players[j]);
-                    }
-                }
-            }
-            console.log(players);
-            return players;
-        }
-    }
-}
