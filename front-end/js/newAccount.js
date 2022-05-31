c992709078870bed7c3568c2e29c5c2ae0b1fd7c
joao <joao.fzm@hotmail.com>
2022-05-31 07:49:57 -0300

    Improve new account registration

--- front-end/js/newAccount.js
+++ front-end/js/newAccount.js
@@ -9,14 +9,26 @@ function register() {
         "username": typedUsername,
         "password": typedPassword
     }
-
     var request = new XMLHttpRequest();
     request.open("POST", "http://localhost:8080/users/", true);
     request.setRequestHeader("Content-type", "application/json");
     request.send(JSON.stringify(body));
-
     request.onload = function () {
         console.log(this.responseText);
+
+        body2 = {
+            "name": typedTeamName,
+            "user": {
+                "id": 3
+            }
+        }
+        var request2 = new XMLHttpRequest();
+        request2.open("POST", "http://localhost:8080/teams/", true);
+        request2.setRequestHeader("Content-type", "application/json");
+        request2.send(JSON.stringify(body2));
+        request2.onload = function () {
+            console.log(this.responseText);
+        }
     }
 
     alert(typedUsername + " registered successfully!")
