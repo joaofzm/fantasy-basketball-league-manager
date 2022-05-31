c456e4dd076705658c0dd88b67830ddec690912b
joao <joao.fzm@hotmail.com>
2022-05-31 08:07:02 -0300

    Finish new account registration implementation

--- front-end/js/newAccount.js
+++ front-end/js/newAccount.js
@@ -4,33 +4,37 @@ function register() {
     var typedUsername = document.getElementById("usernameTextBox").value;
     var typedTeamName = document.getElementById("teamNameTextBox").value;
     var typedPassword = document.getElementById("passwordTextBox").value;
-    
-    body = {
+    postUserBody = {
         "username": typedUsername,
         "password": typedPassword
     }
-    var request = new XMLHttpRequest();
-    request.open("POST", "http://localhost:8080/users/", true);
-    request.setRequestHeader("Content-type", "application/json");
-    request.send(JSON.stringify(body));
-    request.onload = function () {
-        console.log(this.responseText);
 
-        body2 = {
+    var postUserRequest = new XMLHttpRequest();
+    postUserRequest.open("POST", "http://localhost:8080/users/", true);
+    postUserRequest.setRequestHeader("Content-type", "application/json");
+    postUserRequest.send(JSON.stringify(postUserBody));
+    postUserRequest.onload = function () {
+        var userResponseText = this.responseText;
+        var parsedData = JSON.parse(userResponseText);
+        console.log(userResponseText);
+        
+
+        postTeamBody = {
             "name": typedTeamName,
             "user": {
-                "id": 3
+                "id": parsedData.id
             }
         }
-        var request2 = new XMLHttpRequest();
-        request2.open("POST", "http://localhost:8080/teams/", true);
-        request2.setRequestHeader("Content-type", "application/json");
-        request2.send(JSON.stringify(body2));
-        request2.onload = function () {
+        var postTeamRequest = new XMLHttpRequest();
+        postTeamRequest.open("POST", "http://localhost:8080/teams/", true);
+        postTeamRequest.setRequestHeader("Content-type", "application/json");
+        postTeamRequest.send(JSON.stringify(postTeamBody));
+        postTeamRequest.onload = function () {
             console.log(this.responseText);
         }
     }
 
     alert(typedUsername + " registered successfully!")
-    return request.responseText;
+    // window.location.href = "/index.html";
+    return postUserRequest.responseText;
 }
