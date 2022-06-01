d8803d20eebbed98732de87d971f5a541418352e
joao <joao.fzm@hotmail.com>
2022-06-01 05:07:15 -0300

    Make impossible to register a new account with an username already in use

--- front-end/js/newAccount.js
+++ front-end/js/newAccount.js
@@ -4,37 +4,65 @@ function register() {
     var typedUsername = document.getElementById("usernameTextBox").value;
     var typedTeamName = document.getElementById("teamNameTextBox").value;
     var typedPassword = document.getElementById("passwordTextBox").value;
-    postUserBody = {
-        "username": typedUsername,
-        "password": typedPassword
-    }
 
-    var postUserRequest = new XMLHttpRequest();
-    postUserRequest.open("POST", "http://localhost:8080/users/", true);
-    postUserRequest.setRequestHeader("Content-type", "application/json");
-    postUserRequest.send(JSON.stringify(postUserBody));
-    postUserRequest.onload = function () {
-        var userResponseText = this.responseText;
-        var parsedData = JSON.parse(userResponseText);
-        console.log(userResponseText);
-        
+    var getAllUsersRequest = new XMLHttpRequest();
+    getAllUsersRequest.open('get', "http://localhost:8080/users/", true);
+    getAllUsersRequest.setRequestHeader('Content-Type', 'text/plain');
+    getAllUsersRequest.send();
 
-        postTeamBody = {
-            "name": typedTeamName,
-            "user": {
-                "id": parsedData.id
+    getAllUsersRequest.onreadystatechange = function () {
+        if (getAllUsersRequest.readyState === 4) {
+            var getAllUsersRequestResponseText = getAllUsersRequest.responseText;
+            var parsedData = JSON.parse(getAllUsersRequestResponseText);
+
+            var usernameAlreadyInUse = false;
+            for (var i = 0; i < parsedData.length; i++) {
+                if (typedUsername == parsedData[i].username) {
+                    usernameAlreadyInUse = true;
+                }
+            }
+            if (!usernameAlreadyInUse) {
+
+                postUserBody = {
+                    "username": typedUsername,
+                    "password": typedPassword
+                }
+
+                var postUserRequest = new XMLHttpRequest();
+                postUserRequest.open("POST", "http://localhost:8080/users/", true);
+                postUserRequest.setRequestHeader("Content-type", "application/json");
+                postUserRequest.send(JSON.stringify(postUserBody));
+
+                postUserRequest.onload = function () {
+                    var userResponseText = this.responseText;
+                    var parsedData = JSON.parse(userResponseText);
+                    console.log(userResponseText);
+
+                    postTeamBody = {
+                        "name": typedTeamName,
+                        "user": {
+                            "id": parsedData.id
+                        }
+                    }
+
+                    var postTeamRequest = new XMLHttpRequest();
+                    postTeamRequest.open("POST", "http://localhost:8080/teams/", true);
+                    postTeamRequest.setRequestHeader("Content-type", "application/json");
+                    postTeamRequest.send(JSON.stringify(postTeamBody));
+                    postTeamRequest.onload = function () {
+                        console.log(this.responseText);
+                    }
+                }
+
+                alert(typedUsername + " registered successfully!")
+                return postUserRequest.responseText;
+          
+            } else {
+                alert("This username is already in use, try a different one!")
             }
         }
-        var postTeamRequest = new XMLHttpRequest();
-        postTeamRequest.open("POST", "http://localhost:8080/teams/", true);
-        postTeamRequest.setRequestHeader("Content-type", "application/json");
-        postTeamRequest.send(JSON.stringify(postTeamBody));
-        postTeamRequest.onload = function () {
-            console.log(this.responseText);
-        }
     }
 
-    alert(typedUsername + " registered successfully!")
-    // window.location.href = "/index.html";
-    return postUserRequest.responseText;
 }
+
+
