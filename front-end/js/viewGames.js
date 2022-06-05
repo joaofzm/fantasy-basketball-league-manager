2de01cb53abd7ff764bd89d082da7b2a75e5d2ae
joao <joao.fzm@hotmail.com>
2022-06-05 09:38:07 -0300

    Add functionality to filter game from specific player to view games page

--- front-end/js/viewGames.js
+++ front-end/js/viewGames.js
@@ -27,7 +27,8 @@ function loadViewGamesPage() {
             document.getElementById('playersSelect').appendChild(option);
             for (var i = 0; i < players.length; i++) {
                 var option = document.createElement('option');
-                option.value = players[i].name;
+                //Value stores the player id, for later searches
+                option.value = players[i].id;
                 option.innerHTML = players[i].name;
                 document.getElementById('playersSelect').appendChild(option);
             }
@@ -36,14 +37,13 @@ function loadViewGamesPage() {
 
     //Second request prints data
     var printAllPlayersGamesRequest = new XMLHttpRequest();
-    printAllPlayersGamesRequest.open('get', "http://localhost:8080/teams/"+localStorage.getItem("currentLoggedTeamId"), true);
+    printAllPlayersGamesRequest.open('get', "http://localhost:8080/teams/" + localStorage.getItem("currentLoggedTeamId"), true);
     printAllPlayersGamesRequest.setRequestHeader('Content-Type', 'text/plain');
     printAllPlayersGamesRequest.send();
     printAllPlayersGamesRequest.onreadystatechange = function () {
         if (printAllPlayersGamesRequest.readyState === 4) {
             var printAllPlayersGamesRequestResponseText = printAllPlayersGamesRequest.responseText;
             var parsedData = JSON.parse(printAllPlayersGamesRequestResponseText);
-            console.log(parsedData);
 
             for (var i = 0; i < parsedData.players.length; i++) {
                 for (var j = 0; j < parsedData.players[i].games.length; j++) {
@@ -51,13 +51,13 @@ function loadViewGamesPage() {
                     var p = document.createElement("p");
 
                     p.innerHTML =
-                    parsedData.players[i].name + ": "
-                    + parsedData.players[i].games[j].points + " PTS | "
-                    + parsedData.players[i].games[j].assists + " AST | "
-                    + parsedData.players[i].games[j].rebounds + " REB | "
-                    + parsedData.players[i].games[j].blocks + " BLK | "
-                    + parsedData.players[i].games[j].steals + " STL";
-                    
+                        parsedData.players[i].name + ": "
+                        + parsedData.players[i].games[j].points + " PTS | "
+                        + parsedData.players[i].games[j].assists + " AST | "
+                        + parsedData.players[i].games[j].rebounds + " REB | "
+                        + parsedData.players[i].games[j].blocks + " BLK | "
+                        + parsedData.players[i].games[j].steals + " STL";
+
                     div.append(p);
 
                     console.log(parsedData.players[i].games[j]);
@@ -66,6 +66,47 @@ function loadViewGamesPage() {
         }
     }
 
+}
+
+
+
+function reloadPage() {
+
+    if (document.getElementById("playersSelect").value=="All Players"){
+        loadViewGamesPage();
+        return;
+    }
+
+    //Clean div
+    document.getElementById("view_games_div").innerHTML = "";
+    
+    //Value for the options in select was previously set to it's player corresponding id.
+    var selectedPlayerId = document.getElementById("playersSelect").value;
+
+    var getAllGamesFromSelectedPlayerRequest = new XMLHttpRequest();
+    getAllGamesFromSelectedPlayerRequest.open('get', "http://localhost:8080/players/" + selectedPlayerId, true);
+    getAllGamesFromSelectedPlayerRequest.setRequestHeader('Content-Type', 'text/plain');
+    getAllGamesFromSelectedPlayerRequest.send();
+    getAllGamesFromSelectedPlayerRequest.onreadystatechange = function () {
+        if (getAllGamesFromSelectedPlayerRequest.readyState === 4) {
+            var getAllGamesFromSelectedPlayerRequestResponseText = getAllGamesFromSelectedPlayerRequest.responseText;
+            var parsedData = JSON.parse(getAllGamesFromSelectedPlayerRequestResponseText);
+
+            for (var i = 0; i < parsedData.games.length; i++) {
+                var div = document.getElementById('view_games_div');
+                var p = document.createElement("p");
+
+                p.innerHTML =
+                    parsedData.name + ": "
+                    + parsedData.games[i].points + " PTS | "
+                    + parsedData.games[i].assists + " AST | "
+                    + parsedData.games[i].rebounds + " REB | "
+                    + parsedData.games[i].blocks + " BLK | "
+                    + parsedData.games[i].steals + " STL";
+                div.append(p);
+            }
+        }
+    }
 }
 
 
